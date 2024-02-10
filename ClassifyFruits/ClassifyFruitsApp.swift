0c3ab343a8d405e9c1dfe0f65044a11a36f82f3b
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2024-02-10 18:08:26 +0700

    Initial Commit

---
+++ ClassifyFruits/ClassifyFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassifyFruitsApp.swift
+//  ClassifyFruits
+//
+//  Created by Wongkraiwich Chuenchomphu on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ClassifyFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassifyFruits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClassifyFruits
+//
+//  Created by Wongkraiwich Chuenchomphu on 10/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
